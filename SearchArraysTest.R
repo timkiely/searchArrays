f6836e73b22079305ab17befb4eec8423eda1b66
Tim Kiely <timothy.j.kiely@gmail.com>
2014-05-29 18:23:45 -0400

    Text edits to SearchArraysTest

--- SearchArraysTest.R
+++ SearchArraysTest.R
@@ -1,17 +1,16 @@
 
+##This file is for testing the searchArrays function in searchArrays.R
 
-source(paste0(getwd(),"/","searchArrays.R"))
-##for testing
+path<-(paste0(getwd(),"/","searchArrays"))
+setwd(path)
 
-#testing with no common element
+# 1) error testing for no common element
+test<-list()
 test<-list(letters[1:5],letters[6:10],letters[11:15])
+searchArrays(test)
 
-#testing normal arrays
+# 2) testing set of arrays m with a common smallest element 
 test.gen<<-function(n){sort(sample(letters,n,replace=T))}; 
 test<-list()
-for(i in 1:5){
-  a<-list(test.gen(100))
-  test<-append(test,a)
-}
-
+for(i in 1:5){a<-list(test.gen(100));test<-append(test,a)}
 searchArrays(test)
